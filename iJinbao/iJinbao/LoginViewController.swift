b7965d6ef3928bad9be040526b041cd2af2fa519
Guangyu Wang <guangyu.wangg@yahoo.com>
2016-12-28 14:04:44 -0500

    Added Alamofire framework

---
+++ iJinbao/iJinbao/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  iJinbao
+//
+//  Created by Guangyu Wang on 2016-12-28.
+//  Copyright © 2016 jinbao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var userNameTextfield: UITextField!
+    @IBOutlet weak var passwordTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    
+    @IBAction func loginClicked(_ sender: Any) {
+        
+    }
+}
---
+++ iJinbao/iJinbao/RegisterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewCRegisterViewController.swift
+//  iJinbao
+//
+//  Created by Guangyu Wang on 2016-12-28.
+//  Copyright © 2016 jinbao. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func sendRegistration(_ sender: Any) {
+        
+    }
+    
+}
